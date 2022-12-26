d871bf0c33767b08dda737d3200bb0296a92d495
Nathan Evans <nathan@de1ux.com>
2022-12-26 09:52:09 -0700

    Update migrations

--- hive/api/models/item_transition.py
+++ hive/api/models/item_transition.py
@@ -4,6 +4,9 @@ from hive.api.models.item import Item
 
 class ItemTransition(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
     from_location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='from_location')
     to_location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='to_location')
+
+    created_at = models.DateTimeField(auto_now_add=True)
 
